ffd36e61a72e72fc739da603d91ae7f7437ab7c7
Joseph.Hess@Stonybrook.edu <Joseph.Hess@Stonybrook.edu>
2025-04-21 13:03:48 -0400

    Created shell for error popover:

---
+++ frontend/src/Components/ErrorPopover.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface ErrorPopoverProps {
+  show: boolean;
+  message: string;
+  closePopup: () => void;
+}
+
+const ErrorPopover: React.FC<ErrorPopoverProps> = (props) => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default ErrorPopover;
