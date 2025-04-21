cc74059dc4e527bd4d2171e82841fa12aadf4a78
Joseph.Hess@Stonybrook.edu <Joseph.Hess@Stonybrook.edu>
2025-04-21 11:16:36 -0400

    Created shell for game result popover

---
+++ frontend/src/Components/GameOverPopover.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface GameOverPopoverProps {
+  won: boolean;
+  tries: number;
+  correct: string;
+}
+
+const GameOverPopover: React.FC<GameOverPopoverProps> = (props) => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default GameOverPopover;
